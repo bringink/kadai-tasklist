6cba60865f64c776d83ed893bf4dfb259bd640ae
bringink <bringink@gmail.com>
2018-12-08 10:14:12 +0000

    bug fix2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :current_user, only: [:destroy]
+  before_action :current_user, only: [:new, :index, :edit, :destroy]
 
   def index
     @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
